6ed8c453ae51e32d79316443d0fc3f1f77b6c7d2
Knight <karvin_jimenez@hotmail.com>
2024-07-17 23:00:35 -0400

    extra changes.

--- main.cpp
+++ main.cpp
@@ -514,8 +514,14 @@ void render()
     SDL_SetRenderDrawColor(renderer, 29, 29, 27, 255);
     SDL_RenderClear(renderer);
 
+    SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreBounds.w, &scoreBounds.h);
+    scoreBounds.x = 100;
+    scoreBounds.y = scoreBounds.h / 2;
     SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreBounds);
 
+    SDL_QueryTexture(liveTexture, NULL, NULL, &liveBounds.w, &liveBounds.h);
+    liveBounds.x = 300;
+    liveBounds.y = liveBounds.h / 2;
     SDL_RenderCopy(renderer, liveTexture, NULL, &liveBounds);
 
     if (isGamePaused)
@@ -616,17 +622,7 @@ int main(int argc, char *args[])
     fontSquare = TTF_OpenFont("square_sans_serif_7.ttf", 16);
 
     updateTextureText(scoreTexture, "Score: 0");
-
-    SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreBounds.w, &scoreBounds.h);
-    scoreBounds.x = 100;
-    scoreBounds.y = scoreBounds.h / 2;
-
-    updateTextureText(liveTexture, "Lives: 2");
-
-    SDL_QueryTexture(liveTexture, NULL, NULL, &liveBounds.w, &liveBounds.h);
-    liveBounds.x = 300;
-    liveBounds.y = liveBounds.h / 2;
-
+    updateTextureText(liveTexture, "Lives: 2");    
     updateTextureText(pauseTexture, "Game Paused");
 
     SDL_QueryTexture(pauseTexture, NULL, NULL, &pauseBounds.w, &pauseBounds.h);
